fafb3664dd6f10f88240f8992c2c269622a66838
JC Software Solution <engjcsoftwaresolution@gmail.com>
2024-09-04 11:36:02 +0530

    code merged

--- src/environment/index.js
+++ src/environment/index.js
@@ -1,12 +1,14 @@
 const environment = {
-  api: 'http://195.35.8.196:6085/',
-  chat_api: 'http://195.35.8.196:8085/',
-  map_api_key: '',
-  userRoleId: '66d6bdf107668a7723efdf9a',
-  adminRoleId: '66d6bdf107668a7723efdf99',
-  staffRoleId: '66d6e50c3ddf17b1e36f5e6d',
-  contractorRoleId: '66d6e4ff3ddf17b1e36f5e6c',
-  supplierRoleId: '66d6e5163ddf17b1e36f5e6e',
+  api: process.env.REACT_APP_API_URL,
+  chat_api: process.env.REACT_APP_API_URL,
+  userRoleId: process.env.REACT_APP_USER_ROLE_ID,
+  adminRoleId: process.env.REACT_APP_ADMIN_ROLE_ID,
+  staffRoleId:process.env.REACT_APP_STAFF_ROLE_ID,
+  contractorRoleId:process.env.REACT_APP_CONTRACTOR_ROLE_ID,
+  supplierRoleId:process.env.REACT_APP_SUPPLIER_ROLE_ID,
+  map_api_key: process.env.REACT_APP_MAP_API_KEY,
 };
+
+console.log("process.env",process.env)
 
 export default environment;
