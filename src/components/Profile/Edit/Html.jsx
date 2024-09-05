bb677397f961e16886082b64cb527a4447c9f418
JC Software Solution <engjcsoftwaresolution@gmail.com>
2024-09-05 12:41:08 +0530

    code mere

--- src/components/Profile/Edit/Html.jsx
+++ src/components/Profile/Edit/Html.jsx
@@ -111,7 +111,7 @@ const Html = ({
             </div> */}
           </div>
           <div className="text-right mt-3">
-            <button className="text-white bg-gray-400 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-3">
+            <button type="button" className="text-white bg-gray-400 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-3">
               Cancel
             </button>
             <button className="text-white bg-[#063688] bg-[#063688] focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
